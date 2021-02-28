5c9a537115ad2bc7e837c5e42da9e02adcc6e0ce
Lena <lenazurich@gmail.com>
2021-02-28 11:42:56 +0100

    Base test was added to the project

--- src/test/java/baseTest/BaseTest.java
+++ src/test/java/baseTest/BaseTest.java
@@ -1,4 +1,44 @@
 package baseTest;
 
+import org.apache.log4j.Logger;
+import org.junit.After;
+import org.junit.Assert;
+import org.junit.Before;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import java.io.File;
+import java.util.concurrent.TimeUnit;
+
 public class BaseTest {
+    public WebDriver webDriver;
+    protected Logger logger = Logger.getLogger(getClass());
+
+
+    @Before
+    public void setUp(){
+        // for mac OS
+        // File fileFF = new File("./drivers/chromedriver");
+        // for Win
+        File fileFF = new File(".drivers/chromedriver");
+        System.setProperty("webdriver.chrome.driver",fileFF.getAbsolutePath());
+        webDriver = new ChromeDriver();
+
+        webDriver.manage().window().maximize();
+        webDriver.manage().timeouts().implicitlyWait(5, TimeUnit.SECONDS);
+        logger.info("Browser was opened");
+
+
+    }
+
+    @After
+    public void tearDown(){
+        webDriver.quit();
+        logger.info("Browser was closed");
+    }
+
+    protected void checkExpectedResult(String message, boolean actualResult){
+        Assert.assertTrue(message, actualResult);
+    }
+
 }
