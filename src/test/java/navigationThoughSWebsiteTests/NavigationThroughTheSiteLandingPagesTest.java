5b04dc42daab9545b0c34d71fdb3e7fa235e0794
Lena <lenazurich@gmail.com>
2021-03-13 12:27:11 +0100

    ValidationOfLoginPageTest with parameters

--- src/test/java/navigationThoughSWebsiteTests/NavigationThroughTheSiteLandingPagesTest.java
+++ src/test/java/navigationThoughSWebsiteTests/NavigationThroughTheSiteLandingPagesTest.java
@@ -1,10 +1,12 @@
 package navigationThoughSWebsiteTests;
 
 import baseTest.BaseTest;
+import org.junit.Ignore;
 import org.junit.Test;
 
 public class NavigationThroughTheSiteLandingPagesTest extends BaseTest {
 
+    @Ignore
     @Test
     public void checkOfNavigationMainMenu(){
 
--- src/test/java/registration/ValidationOfLoginPageTest.java
+++ src/test/java/registration/ValidationOfLoginPageTest.java
@@ -1,9 +1,24 @@
 package registration;
 
 import baseTest.BaseTest;
+import io.qameta.allure.*;
+import junitparams.JUnitParamsRunner;
+import junitparams.Parameters;
+import junitparams.naming.TestCaseName;
 import org.junit.Test;
+import org.junit.runner.RunWith;
 
+@RunWith(JUnitParamsRunner.class)
+@Epic("Allure examples")
+@Feature("Junit 4 support")
 public class ValidationOfLoginPageTest extends BaseTest {
+    @Description("Some detailed test description")
+    @Story("Base support for bdd annotations")
+    @Link("https://example.org")
+    @Link(name = "allure", type = "mylink")
+    @Issue("123")
+    @Issue("432")
+    @Severity(SeverityLevel.CRITICAL)
     @Test
     public void validationRegistrationSpaceSymbols()  {
         loginPage.openLoginPage();
@@ -22,6 +37,20 @@ public class ValidationOfLoginPageTest extends BaseTest {
         loginPage.clickButtonLogIn();
         loginPage.checkErrorsMessages("Enter your email address;Space symbols is prohibited");
 
+    }
+
+    @Test
+    @Parameters({
+            "Enter your email address;Space symbols is prohibited,  , password with space symbols",
+            "Enter a valid email address using the following format: myname@mail.com;Password must contain at least 6 characters,invalid.email.com, 12"
+    })
+    @TestCaseName("validationRegistration: errorMessages = {1}, email = {2}, pass = {3}")
+    public  void validationRegistration(String errorMessages, String email, String pass){
+        loginPage.openLoginPage();
+        loginPage.enterUserEmail(email);
+        loginPage.enterUserPass(pass);
+        loginPage.clickButtonLogIn();
+        loginPage.checkErrorsMessages(errorMessages);
     }
 
 }
